fa13be074c2e4ebdc6fab3e26a5a68db0344b5e0
Jechol Lee <mr.jechol@gmail.com>
2021-07-30 16:56:52 +0900

    fail

--- test/use_cases/nested_call_test.exs
+++ test/use_cases/nested_call_test.exs
@@ -1,14 +1,18 @@
 defmodule Defre.NestedCallTest do
   use ExUnit.Case, async: true
   use Witchcraft, override_kernel: false
-  alias Algae.Either.{Left, Right}
+  import Defre
+  alias Algae.Either.Right
   alias Algae.Reader
 
   defmodule User do
     defstruct [:id, :src]
 
     def get_by_id(user_id) do
-      %__MODULE__{id: user_id, src: :db} |> Right.new()
+      monad %Reader{} do
+        env <- Reader.ask()
+        return(%__MODULE__{id: user_id, src: Map.get(env, :src, :db)} |> Right.new())
+      end
     end
   end
 
@@ -21,13 +25,20 @@ defmodule Defre.NestedCallTest do
   defmodule UserController do
     def profile(user_id) do
       monad %Reader{} do
+        env <- Reader.ask()
+
         return(
           monad %Right{} do
-            %User{id: id, src: src} <- Accounts.get_user_by_id(user_id)
+            %User{id: id, src: src} <- Accounts.get_user_by_id(user_id) |> Reader.run(env)
             "id: #{id}, src: #{src}" |> Right.new()
           end
         )
       end
+    end
+
+    defre profile_re(user_id) do
+      %User{id: id, src: src} <- Accounts.get_user_by_id(user_id)
+      "id: #{id}, src: #{src}" |> Right.new()
     end
   end
 
