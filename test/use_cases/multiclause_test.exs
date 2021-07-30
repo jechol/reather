82d96703f491ac14c127f88902fc90a8bdbf21da
Jechol Lee <mr.jechol@gmail.com>
2021-07-30 18:06:19 +0900

    MulticlauseTest

--- test/use_cases/multiclause_test.exs
+++ test/use_cases/multiclause_test.exs
@@ -1,5 +1,6 @@
 defmodule Defre.MulticlauseTest do
   use ExUnit.Case, async: true
+  alias Algae.Reader
   import Defre
 
   defmodule Multi do
@@ -8,15 +9,10 @@ defmodule Defre.MulticlauseTest do
   end
 
   test "Multiclause with pattern matching works" do
-    assert Multi.add(10, 0) == 10
-    assert Multi.add(10, 0, mock(%{&Calc.id/1 => 99})) == 99
+    assert Multi.add(10, 0) |> Reader.run(%{}) == 10
+    assert Multi.add(10, 0) |> Reader.run(mock(%{&Calc.id/1 => 99})) == 99
 
-    assert Multi.add(10, 1) == 11
-
-    assert_raise RuntimeError, ~r/unused/, fn ->
-      Multi.add(10, 1, mock(%{&Calc.id/1 => 99}))
-    end
-
-    assert Multi.add(10, 1, mock(%{&Calc.sum/2 => 99})) == 99
+    assert Multi.add(10, 1) |> Reader.run(%{}) == 11
+    assert Multi.add(10, 1) |> Reader.run(mock(%{&Calc.sum/2 => 99})) == 99
   end
 end
