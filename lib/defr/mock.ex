8207218a15bec252efd8c8fea050626237674cd1
Jechol Lee <mr.jechol@gmail.com>
2021-07-31 11:28:45 +0900

    remove module decoration

--- lib/defr/mock.ex
+++ lib/defr/mock.ex
@@ -1,14 +1,6 @@
 defmodule Defr.Mock do
   @moduledoc false
 
-  def decorate_with_fn({{:__aliases__, _, _}, _} = orig) do
-    orig
-  end
-
-  def decorate_with_fn({mod, _} = orig) when is_atom(mod) do
-    orig
-  end
-
   def decorate_with_fn({{:&, _, [{:/, _, [_mf, a]}]} = capture, v}) do
     const_fn = {:fn, [], [{:->, [], [Macro.generate_arguments(a, __MODULE__), v]}]}
     {capture, const_fn}
