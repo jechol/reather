357ec5b86c42f4b8573e21489ff5a4bd35f841fe
Jechol Lee <mr.jechol@gmail.com>
2021-08-03 13:19:27 +0900

    Simplify tests

--- test/use_cases/nested_reader_test.exs
+++ test/use_cases/nested_reader_test.exs
@@ -2,7 +2,6 @@ defmodule Defr.NestedCallTest do
   use ExUnit.Case, async: true
   use Defr
   alias Algae.Reader
-  alias Algae.Either.Right
 
   defmodule Repo do
     def get(_schema, _id) do
@@ -10,62 +9,36 @@ defmodule Defr.NestedCallTest do
     end
   end
 
+  defmodule Password do
+    def validate(pw, pw_hash) do
+      :crypto.hash(:sha3_256, pw) == pw_hash
+    end
+  end
+
   defmodule User do
     use Defr
 
-    defstruct [:id, :name]
+    defstruct [:id, :pw_hash]
 
     defr get_by_id(user_id) do
-      Repo.get(__MODULE__, user_id) |> Right.new()
+      Repo.get(__MODULE__, user_id)
     end
   end
 
   defmodule Accounts do
     use Defr
 
-    defr get_user_by_id(user_id) do
-      monad %Right{} do
-        user <- User.get_by_id(user_id)
-        user |> Right.new()
-      end
+    defr sign_in(user_id, pw) do
+      user = User.get_by_id(user_id)
+      Password.validate(pw, user.pw_hash)
     end
   end
 
-  defmodule UserController do
-    use Defr
-
-    defr profile(user_id_str) do
-      user_id = String.to_integer(user_id_str)
-      Accounts.get_user_by_id(user_id)
-    end
-  end
-
-  test "inject 3rd layer" do
-    assert [{:profile, 1}] == UserController.__defr_funs__()
-
-    assert %Right{right: %User{id: 1, name: "josevalim"}} ==
-             UserController.profile("1")
-             |> Reader.run(%{&Repo.get/2 => fn _, _ -> %User{id: 1, name: "josevalim"} end})
-  end
-
-  test "inject 2nd layer" do
-    assert [{:get_user_by_id, 1}] == Accounts.__defr_funs__()
-
-    assert %Right{right: %User{id: 2, name: "chrismccord"}} ==
-             UserController.profile("2")
-             |> Reader.run(%{
-               &User.get_by_id/1 => fn _ ->
-                 Reader.new(fn _ -> Right.new(%User{id: 2, name: "chrismccord"}) end)
-               end
-             })
-
-    # with `mock` macro
-    assert %Right{right: %User{id: 2, name: "chrismccord"}} ==
-             UserController.profile("2")
+  test "Accounts.sign_in" do
+    assert true ==
+             Accounts.sign_in(100, "hello")
              |> Reader.run(
-               mock(%{
-                 &User.get_by_id/1 => Right.new(%User{id: 2, name: "chrismccord"})
-               })
+               mock(%{&Repo.get/2 => %User{id: 100, pw_hash: :crypto.hash(:sha3_256, "hello")}})
              )
   end
 end
