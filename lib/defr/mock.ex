ea9b176ec08ed3d6c7de829ccfa28724f54120e5
Trevor Lee <trevor@momenti.tv>
2021-09-27 21:12:33 +0900

    Code.ensure_loaded!

--- lib/defr/mock.ex
+++ lib/defr/mock.ex
@@ -19,6 +19,8 @@ defmodule Defr.Mock do
 
     value =
       quote do
+        Code.ensure_loaded!(unquote(m))
+
         Defr.Mock.select(
           {unquote(m), unquote(f), unquote(a)},
           unquote(const_fn),
