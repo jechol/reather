2682e7d60291c190e118e00eddc255c8401bb755
Jechol Lee <jechol@momenti.tv>
2022-01-21 20:25:51 +0900

    format

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,15 @@
 # Used by "mix format"
 
-locals_without_parens = [let: 1, defr: 1, defr: 2, defrp: 2, assert_ast: 1]
+locals_without_parens = [
+  let: 1,
+  defr: 1,
+  defr: 2,
+  defrp: 2,
+  defri: 1,
+  defri: 2,
+  defrip: 2,
+  assert_ast: 1
+]
 
 [
   locals_without_parens: locals_without_parens,
