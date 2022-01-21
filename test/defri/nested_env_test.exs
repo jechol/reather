7735cc83121e8cfe2c28944a0cfa8a07c09adef0
Jechol Lee <jechol@momenti.tv>
2022-01-21 21:38:01 +0900

    nested env

--- test/defri/nested_env_test.exs
+++ test/defri/nested_env_test.exs
@@ -14,14 +14,14 @@ defmodule Defri.NestedEnvTest do
       bottom() |> run()
     end
 
-    defrp bottom() do
+    defrip bottom() do
       %{value: {source, value}} <- ask()
-      {source, value}
+      {source, value} |> Right.new()
     end
   end
 
   test "outer env has higher priority" do
-    assert {:env, 10} = Target.top(10) |> Rither.run(%{})
-    assert {:mock, 20} = Target.top(10) |> Rither.run(%{value: {:mock, 20}})
+    assert %Right{right: {:env, 10}} = Target.top(10) |> Rither.run(%{})
+    assert %Right{right: {:mock, 20}} = Target.top(10) |> Rither.run(%{value: {:mock, 20}})
   end
 end
