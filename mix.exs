f41ae14e623f8c8c5d78c406360e3ce5baa063ee
Jechol Lee <jechol@momenti.tv>
2022-02-24 17:24:08 +0900

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -11,6 +11,13 @@ defmodule Reather.MixProject do
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       package: package(),
       description: "Either transformed Reader (reather = REAder + eiTHER)",
       source_url: "https://github.com/jechol/reather",
@@ -33,7 +40,8 @@ defmodule Reather.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.27.0", only: :dev, runtime: false},
-      {:algae, "~> 1.3"}
+      {:algae, "~> 1.3"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
