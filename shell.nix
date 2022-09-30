323aaf5838846e35df36b8890df2029b1ba4627e
Jechol Lee <mr.jechol@gmail.com>
2022-09-30 18:11:50 +0900

    update elixir

--- shell.nix
+++ shell.nix
@@ -1,15 +1,24 @@
 let
-  nixpkgs = import (fetchTarball {
-    url = "https://github.com/jechol/nixpkgs/archive/21.11-otp24-no-jit.tar.gz";
-    sha256 = "sha256:1lka707hrnkp70vny99m9fmp4a8136vl7addmpfsdvkwb81d1jk9";
-  }) { };
-  platform = if nixpkgs.stdenv.isDarwin then [
-    nixpkgs.darwin.apple_sdk.frameworks.CoreServices
-    nixpkgs.darwin.apple_sdk.frameworks.Foundation
-  ] else if nixpkgs.stdenv.isLinux then
-    [ nixpkgs.inotify-tools ]
-  else
-    [ ];
-in nixpkgs.mkShell {
-  buildInputs = with nixpkgs; [ erlang elixir ] ++ platform;
+  nixpkgs = import
+    (fetchTarball {
+      url = "https://github.com/jechol/nixpkgs/archive/22.05-otp25-no-jit.tar.gz";
+      sha256 = "sha256:1k4wgcrffdlr7pr401md6dmvfwawcslvcwkv7qjgqqyrx52v130i";
+    })
+    { };
+  platform =
+    if nixpkgs.stdenv.isDarwin then [
+      nixpkgs.darwin.apple_sdk.frameworks.CoreServices
+      nixpkgs.darwin.apple_sdk.frameworks.Foundation
+    ] else if nixpkgs.stdenv.isLinux then
+      [ nixpkgs.inotify-tools ]
+    else
+      [ ];
+in
+nixpkgs.mkShell {
+  buildInputs = with nixpkgs;
+    [
+      # OTP
+      erlang
+      elixir
+    ] ++ platform;
 }
